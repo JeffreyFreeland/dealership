8cb08869d22edf97fce663f48468b056a39fa570
Jeffrey <>
2020-08-06 18:56:39 -0500

    added top toolbar and the material theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+// TODO: Put these in a material module instead. 
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InventoryModule } from './inventory/inventory.module';
+import { InventoryModule } from './inventory/inventory.module';;
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -21,6 +28,10 @@ import { InventoryModule } from './inventory/inventory.module';
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
     ),
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
