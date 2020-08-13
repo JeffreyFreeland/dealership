468d39857ccbdfa5986f9ac24ea44150c3ef4238
Jeffrey <>
2020-08-12 19:02:29 -0500

    added material module to inventory module

--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -1,6 +1,8 @@
 ﻿import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from '../common/modules/material.module';
+
 import { InventoryRoutingModule } from './inventory-routing.module';
 import { CreateCarComponent } from './create-car/create-car.component';
 import { ListCarsComponent } from './list-cars/list-cars.component';
@@ -14,6 +16,7 @@ import { ListCarsComponent } from './list-cars/list-cars.component';
   imports: [
     CommonModule,
     InventoryRoutingModule,
+    MaterialModule
   ]
 })
 export class InventoryModule { }
